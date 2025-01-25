064b46d2125dd0945dabe747d6863a0008aaa913
JaimeenMakavana <jaimeen.makavana@gmail.com>
2025-01-25 14:02:42 +0530

    minor type issue solved

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,17 +1,17 @@
 "use client";
 
 import useStore from "@/store/store";
-import { useRouter, useSearchParams } from "next/navigation";
+import { usePathname, useSearchParams } from "next/navigation";
 import { useEffect } from "react";
 
 const Template = ({ children }: { children: React.ReactNode }) => {
   const { setRandomIndex } = useStore();
-  const router: any = useRouter();
+  const router = usePathname();
   const params = useSearchParams();
 
   useEffect(() => {
     setRandomIndex();
-  }, [router.pathname, params.get("id")]);
+  }, [router, params.get("id")]);
 
   return <>{children}</>;
 };
